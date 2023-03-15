ce5bdb0e52aebae125163aa94a9aede6a47fd3fd
Darshanamishra <darshanamishra25@gamil.com>
2023-03-15 15:32:48 -0600

    First Commit

--- src/main/java/OrangeHRM/Initialize.java
+++ src/main/java/OrangeHRM/Initialize.java
@@ -1,12 +1,9 @@
 package OrangeHRM;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import jdk.jfr.Timespan;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
@@ -15,20 +12,21 @@ import java.time.Duration;
 
 public class Initialize {
     WebDriver d;
-   WebDriverWait wait;
+    WebDriverWait wait;
+
     @BeforeTest()
     public void Initialization() throws Exception {
-        ChromeOptions options=new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriverManager.chromedriver().setup();
         d = new ChromeDriver(options);
-        wait = new WebDriverWait(d,Duration.ofSeconds(30));
+        wait = new WebDriverWait(d, Duration.ofSeconds(30));
         d.manage().window().maximize();
     }
+
     @AfterTest
-    public void TearDown()
-    {
+    public void TearDown() {
         d.quit();
     }
 
--- src/main/java/OrangeHRM/Login.java
+++ src/main/java/OrangeHRM/Login.java
@@ -1,27 +1,81 @@
 package OrangeHRM;
 
-import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.concurrent.TimeUnit;
 
 import java.time.Duration;
 
 public class Login extends Initialize {
-
-    @Deprecated
-    @Test(priority = 1)
-    public void login() throws Exception{
+    //Login Module with Positive/Negative TestCases
+    @Test(priority = 6)
+    public void ValidLogin() throws Exception {
         d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
         d.findElement(By.name("username")).sendKeys("Admin");
-        d.findElement(By.name("password")).sendKeys("admin123");
-        d.findElement(By.name("password")).sendKeys(Keys.ENTER);
-        d.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        d.findElement(By.name("password")).sendKeys("admin123", Keys.ENTER);
+        Assert.assertEquals(d.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+        System.out.println("Login successfully");
     }
 
+    @Test(priority = 1)
+    public void InvalidUsernameLogin() throws Exception {
+        d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        d.findElement(By.name("username")).sendKeys("Admi");
+        d.findElement(By.name("password")).sendKeys("admin123", Keys.ENTER);
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(d.findElement(By.cssSelector(".oxd-alert-content-text")).isEnabled());
+        System.out.println("Invalid Username");
+    }
+
+    @Test(priority = 2)
+    public void InvalidPassLogin() throws Exception {
+        d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        d.findElement(By.name("username")).sendKeys("Admin");
+        d.findElement(By.name("password")).sendKeys("admin", Keys.ENTER);
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(d.findElement(By.cssSelector(".oxd-alert-content-text")).isEnabled());
+        System.out.println("Invalid Password");
+    }
+
+    @Test(priority = 3)
+    public void InvalidUsernamePassLogin() throws Exception {
+        d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        d.findElement(By.name("username")).sendKeys("Admi");
+        d.findElement(By.name("password")).sendKeys("admi", Keys.ENTER);
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(d.findElement(By.cssSelector(".oxd-alert-content-text")).isEnabled());
+        System.out.println("Invalid Username and Password");
+    }
+
+    @Test(priority = 4)
+    public void InvalidLoginEmptyUsername() throws Exception {
+        d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        d.findElement(By.name("password")).sendKeys("admin123", Keys.ENTER);
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(d.findElement(By.xpath("//form[@class='oxd-form']/div[1]//span[@class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message']")).isEnabled());
+        System.out.println("Username empty");
+    }
+
+    @Test(priority = 5)
+    public void InvalidLoginEmptyPass() throws Exception {
+        d.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        d.findElement(By.name("username")).sendKeys("Admin", Keys.ENTER);
+        d.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        Assert.assertTrue(d.findElement(By.xpath("//form[@class='oxd-form']/div[2]//span[@class='oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message']")).isEnabled());
+        System.out.println("Password empty");
+    }
 }
